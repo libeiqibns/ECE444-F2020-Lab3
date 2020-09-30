93c5c0689a3f0aa6601fc177a21878b00bc09bf5
Beiqi Li <beiqi.li@mail.utoronto.ca>
2020-09-30 17:01:36 +0800

    code added for activity 2

--- hello.py
+++ hello.py
@@ -1,12 +1,21 @@
-from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -19,17 +28,21 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name,
-                           current_time=datetime.utcnow())
-
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        is_uoft_email =  True
+        if session['email'].find('utoronto') < 0:
+            is_uoft_email = False
+        session['UofTmail'] = is_uoft_email
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), UofTmail = session.get('UofTmail'))
 
 if __name__ == '__main__':
     app.run()
